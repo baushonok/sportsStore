a16be8314d0beb42166ea9084f030fbce237e5ef
baushonok <baushonok@gmail.com>
2018-01-27 18:35:51 +0300

    Add shopping cart

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -1,6 +1,9 @@
 import {Component} from '@angular/core';
+import {Router} from '@angular/router';
+
 import {Product} from '../model/product.model';
 import {ProductRepository} from '../model/product.repository';
+import {Cart} from '../model/cart.model';
 
 @Component({
 	selector: "store",
@@ -12,7 +15,9 @@ export class StoreComponent {
 	public productsPerPage = 4;
 	public selectedPage = 1;
 
-	constructor(private repository: ProductRepository) {}
+	constructor(private repository: ProductRepository,
+				private cart: Cart,
+				private router: Router) {}
 
 	get products(): Product[] {
 		let pageIndex = (this.selectedPage - 1) * this.productsPerPage;
@@ -41,9 +46,8 @@ export class StoreComponent {
 		return Math.ceil(this.repository.getProducts(this.selectedCategory).length / this.productsPerPage);
 	}
 
-	// get pageNumbers(): number[] {
-	// 	return Array(Math.ceil(this.repository.getProducts(this.selectedCategory).length / this.productsPerPage))
-	// 											.fill(0)
-	// 											.map((x, i) => i + 1);
-	// }
+	addProductToCart(product: Product) {
+		this.cart.addLine(product);
+		this.router.navigateByUrl('/cart');
+	}
 }
